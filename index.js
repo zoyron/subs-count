0769e6ae63e6e343fa9026533fd7541119b17950
zoyron <sgrarora3@gmail.com>
2020-05-12 22:33:45 +0530

    plotting of subscribers completed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const request = require("request-promise");
 const cheerio = require("cheerio");
 const cron = require("node-cron");
+const asciichart = require("asciichart");
 
 let checkSubscriberCount = async (channelUrl) => {
   let response = await request(channelUrl);
@@ -9,15 +10,18 @@ let checkSubscriberCount = async (channelUrl) => {
     '[class="yt-subscription-button-subscriber-count-branded-horizontal subscribed yt-uix-tooltip"]'
   ).attr("title");
 
-  return subscriberCount;
+  return subscriberCount.replace(/\.+/, ""); // replace all the dots in the subscriber number with nothing
 };
 
 (async () => {
   let youtubeChannelUrl = "https://www.youtube.com/EdSheeran";
+  let subscriberArray = [];
 
-  cron.schedule("* * * * * *", async () => {
+  cron.schedule("*/2 * * * * *", async () => {
     let subs = await checkSubscriberCount(youtubeChannelUrl);
-    console.log(`Time: ${new Date().toLocaleString()}`);
-    console.log(`Subscribers: ${subs}`);
+    subscriberArray.push(subs);
+
+    console.log("\033[2J"); //this line will always clear out the console as soon as the script runs so that we have a much clearer view
+    console.log(asciichart.plot(subscriberArray));
   });
 })();
