8db66e18d1ef5a273883af49a3c6ed01548101f9
zoyron <sgrarora3@gmail.com>
2020-05-12 22:10:14 +0530

    added the node-cron functionality to the project

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const request = require("request-promise");
 const cheerio = require("cheerio");
+const cron = require("node-cron");
 
-(async () => {
-  let youtubeChannelUrl = "https://www.youtube.com/EdSheeran";
-
-  let response = await request(youtubeChannelUrl);
+let checkSubscriberCount = async (channelUrl) => {
+  let response = await request(channelUrl);
   let $ = cheerio.load(response);
   let subscriberCount = $(
     '[class="yt-subscription-button-subscriber-count-branded-horizontal subscribed yt-uix-tooltip"]'
   ).attr("title");
-  console.log(subscriberCount);
+
+  return subscriberCount;
+};
+
+(async () => {
+  let youtubeChannelUrl = "https://www.youtube.com/EdSheeran";
+
+  cron.schedule("* * * * * *", async () => {
+    let subs = await checkSubscriberCount(youtubeChannelUrl);
+    console.log(`Time: ${new Date().toLocaleString()}`);
+    console.log(`Subscribers: ${subs}`);
+  });
 })();
